e79831dbc7681d033149316e73ccc75d940e20c4
thangtea thangzaliana <thangtea.18.dec@gmail.com>
2022-05-26 14:31:57 +0530

    d

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<title>Laravel with Inertia</title>
-		@vite
+		@vite("public/build/manifest.json")
         @routes
 	</head>
 	<body class="antialiased">
